74d49e0e9ec795733b732a116499e119db99de02
Jonathan Katongole <jonathaninnocentkatongolejr@gmail.com>
2025-10-30 00:39:29 +0300

    build.gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,21 +11,24 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
 subprojects {
-    afterEvaluate {
-        if (name == "flutter_bluetooth_serial") {
-            val androidExtension = extensions.findByType(LibraryExtension::class.java)
-            androidExtension?.namespace = "io.github.edufolly.flutter_bluetooth_serial"
+    evaluationDependsOn(":app")
+}
+
+gradle.beforeProject {
+    if (name == "flutter_bluetooth_serial") {
+        plugins.withId("com.android.library") {
+            extensions.configure<LibraryExtension>("android") {
+                namespace = "io.github.edufolly.flutterbluetoothserial"
+            }
         }
     }
 }
