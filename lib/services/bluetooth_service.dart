ea755364d4276cf220520d192007d4ebe33bf3be
Jonathan Katongole <jonathaninnocentkatongolejr@gmail.com>
2025-10-29 23:04:23 +0300

    made by codex

--- lib/services/bluetooth_service.dart
+++ lib/services/bluetooth_service.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import '../models/sensor_record.dart';
