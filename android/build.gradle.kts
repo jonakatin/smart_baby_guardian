d10178f10207fd3b74be21a7d1c64bb59cdda233
Jonathan Katongole <jonathaninnocentkatongolejr@gmail.com>
2025-10-30 05:44:29 +0300

    change

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,3 @@
-import com.android.build.gradle.LibraryExtension
-import org.gradle.api.tasks.Delete
-import org.gradle.api.file.Directory
-
-plugins {
-    id("com.android.application") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 allprojects {
     repositories {
         google()
@@ -19,26 +9,14 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
 subprojects {
-    evaluationDependsOn(":app")
-}
-
-gradle.beforeProject {
-    if (name == "flutter_bluetooth_serial") {
-        plugins.withId("com.android.library") {
-            extensions.configure<LibraryExtension>("android") {
-                namespace = "io.github.edufolly.flutterbluetoothserial"
-            }
-        }
-    }
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
