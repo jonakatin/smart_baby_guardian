27b8e5c7e703d56ea5286474932afc955b2e7735
Katongole Jonathan <67139624+jonakatin@users.noreply.github.com>
2025-10-30 18:25:08 +0300

    Import BluetoothDevice type for widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:smart_baby_guard/main.dart';
-import 'package:smart_baby_guard/services/alert_service.dart';
 import 'package:smart_baby_guard/services/bluetooth_service.dart';
+import 'package:smart_baby_guard/screens/connect_screen.dart';
+
+class _FakeBluetoothService extends BluetoothService {
+  @override
+  Future<void> ensureOn() async {}
+
+  @override
+  Future<List<BluetoothDevice>> discover() async {
+    return [];
+  }
+
+  @override
+  Future<void> connectDevice(BluetoothDevice device) async {}
+}
 
 void main() {
   testWidgets('Smart Baby Guard launches connect screen', (tester) async {
     await tester.pumpWidget(
       MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => BluetoothService()),
-          ChangeNotifierProvider(create: (_) => AlertService()),
+          ChangeNotifierProvider<BluetoothService>(
+              create: (_) => _FakeBluetoothService()),
         ],
-        child: const SmartBabyGuardApp(),
+        child: const MaterialApp(home: ConnectScreen()),
       ),
     );
     await tester.pumpAndSettle();
     expect(find.text('Smart Baby Guard'), findsOneWidget);
-    expect(find.textContaining('Connect to your SmartBabyGuard monitor'),
+    expect(find.textContaining('Select your SmartBabyGuard device to connect'),
         findsOneWidget);
   });
 }
